a4cb37ac99b77e62f63d2c07a0b9eca94871a198
sardeath <61544664+sardeath@users.noreply.github.com>
2020-02-27 12:04:44 +0000

    Add files via upload

---
+++ pecah_tag.js
@@ -0,0 +1,11 @@
+<span style="color:white">Tags : <script type="text/javascript">
+var nn=unescape("%26");
+var q="test 1 2 3";
+var z="";
+q=q.split(" ");
+z=z.split(" ");
+for (i=0;i<q.length;i++)
+{document.write('<a href="/site_tag.xhtml?get-q='+q[i]+'">'+q[i]+'</a>, ')};
+for (i=0;i<z.length;i++)
+{document.write('<a href="/site_tag.xhtml?get-z='+z[i]+'">'+z[i]+'</a>, ')};
+</script></span>
